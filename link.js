40eca25410fb61f55491a392b2dbc79a447b4281
112batman <tijnhoftijzer@gmail.com>
2020-12-07 14:01:52 +0100

    Add more content

--- link.js
+++ link.js
@@ -3,3 +3,9 @@ document.querySelectorAll('div.button[href]').forEach(el => {
         window.location.href = el.getAttribute('href')
     })
 })
+
+document.querySelectorAll('*.href[href]').forEach(el => {
+    el.addEventListener('click', () => {
+        window.location.href = el.getAttribute('href')
+    })
+})
